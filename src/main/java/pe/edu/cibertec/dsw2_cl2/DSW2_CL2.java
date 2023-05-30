ffbc506ab1cd8c226e3e172e05f4ca04e73050ea
Armando lopez Torres <mandoveloper@gmail.com>
2023-05-29 21:51:40 -0500

    Commint inicial

---
+++ src/main/java/pe/edu/cibertec/dsw2_cl2/DSW2_CL2.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.dsw2_cl2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSW2_CL2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSW2_CL2.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/dsw2_cl2/DSW2_CL2.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.dsw2_cl2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSW2_CL2 {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
